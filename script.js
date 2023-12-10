b0fa67e3f8a0c3f87640b55421f12876ded0b581
ItayCoifman <itaycoifman@gmail.com>
2023-12-10 19:06:14 +0200

    updated version

--- script.js
+++ script.js
@@ -1,23 +1,160 @@
+const mainVideoUpload = document.getElementById("main-video-upload");
+const otherVideosUpload = document.getElementById("other-videos-upload");
 const mainVideo = document.getElementById("main-video");
-mainVideo.controls = false; 
+let otherVideos = document.getElementById("other-videos");
+
 
 var videoPlayers = {};
-videoPlayers[mainVideo.id] = mainVideo;
+videoPlayers["main_video"] = "";
 var speed = mainVideo.playbackRate
 
-const videos = [
-{
-        id: "body orientation",
-        src: "animation/animation_body_orientation.mp4"
-      },
-  {
-        id: " Pose",
-        src: "animation/animation_pose.mp4"
-  },
-];
+// Upload videos event listeners
+mainVideoUpload.addEventListener("change", function() {
+  if (this.files && this.files[0]) {
+    // Start paused
+    var url = URL.createObjectURL(this.files[0]);
+    mainVideo.src = url;
+    pauseVideo(mainVideo.id); // Pause the video initially
+    videoPlayers["main_video"] = mainVideo;
+  }
+});
 
+otherVideosUpload.addEventListener("change", function() {
+  // Clear existing videos
+  otherVideos.innerHTML = "";
+
+  if (this.files) {
+    for (let i = 0; i < this.files.length; i++) {
+      var url = URL.createObjectURL(this.files[i]);
+      var videoId = "video" + i;
+      createVideoPlayer(videoId, url);
+      pauseVideo(videoId); // Pause the video initially
+    }
+  }
+});
+
+function createVideoPlayer(videoId, url) {
+  var videoPlayer = document.createElement("video");
+  videoPlayer.id = videoId;
+  videoPlayer.src = url;
+  videoPlayer.controls = false; // Show video controls
+  otherVideos.appendChild(videoPlayer);
+  videoPlayers[videoId] = videoPlayer;
+  changeSpeed(videoId)
+  playVideo(videoId)
+}
+
+
+mainVideo.controls = false;
+
+
+
+
+const sub_videos = [];
+
+// Set the attributes for the file input elements
+sub_videos.forEach(video => {
+  video.input.type = "file";
+  video.input.accept = "video/*";
+  video.input.id = video.id + "-upload";
+  video.input.addEventListener("change", function() {
+    if (this.files && this.files[0]) {
+      var url = URL.createObjectURL(this.files[0]);
+      createVideoPlayer(video.id, url);
+
+    }
+  });
+});
+
+// Create checkboxes for each video in the videos array
+for (var i = 0; i < otherVideos.length; i++) {
+  var checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.id = sub_videos[i].id;
+  checkbox.value = sub_videos[i].id;
+
+  var label = document.createElement("label");
+  label.textContent = sub_videos[i].id;
+
+  checkboxes.appendChild(checkbox);
+  checkboxes.appendChild(label);
+  checkboxes.appendChild(sub_videos[i].input); // Append the file input element to the checkboxes div
+
+  checkbox.addEventListener("change", checked_video);
+}
+
+
+
+// Main Video Drag and Drop
+const mainVideoContainer = document.getElementById("main-video-container");
+mainVideoContainer.addEventListener("dragover", function(event) {
+  event.preventDefault(); // Prevent default behavior (Prevent file from being opened)
+});
+
+mainVideoContainer.addEventListener("drop", function(event) {
+  event.preventDefault(); // Prevent default behavior (Prevent file from being opened)
+
+  if (event.dataTransfer.items) {
+    // Use DataTransferItemList interface to access the file(s)
+    for (var i = 0; i < event.dataTransfer.items.length; i++) {
+      // If dropped items aren't files, reject them
+      if (event.dataTransfer.items[i].kind === "file") {
+        var file = event.dataTransfer.items[i].getAsFile();
+        var url = URL.createObjectURL(file);
+        mainVideo.src = url;
+        videoPlayers["main_video"] = mainVideo;
+      }
+    }
+  } else {
+    // Use DataTransfer interface to access the file(s)
+    for (var i = 0; i < event.dataTransfer.files.length; i++) {
+      var url = URL.createObjectURL(event.dataTransfer.files[i]);
+      mainVideo.src = url;
+      videoPlayers["main_video"] = mainVideo;
+    }
+  }
+});
+
+// Assuming the container for other videos has an ID "other-videos"
+const otherVideosContainer = document.getElementById("other-videos");
+
+otherVideosContainer.addEventListener("dragover", function(event) {
+  event.preventDefault(); // Prevent default behavior (Prevent file from being opened)
+});
+
+otherVideosContainer.addEventListener("drop", function(event) {
+  event.preventDefault(); // Prevent default behavior (Prevent file from being opened)
+
+  // Clear existing videos
+  otherVideos.innerHTML = "";
+
+  if (event.dataTransfer.items) {
+    // Use DataTransferItemList interface to access the file(s)
+    for (var i = 0; i < event.dataTransfer.items.length; i++) {
+      // If dropped items aren't files, reject them
+      if (event.dataTransfer.items[i].kind === "file") {
+        var file = event.dataTransfer.items[i].getAsFile();
+        var url = URL.createObjectURL(file);
+        var videoId = "video" + i;
+        createVideoPlayer(videoId, url);
+        pauseVideo(videoId); // Pause the video initially
+      }
+    }
+  } else {
+    // Use DataTransfer interface to access the file(s)
+    for (var i = 0; i < event.dataTransfer.files.length; i++) {
+      var url = URL.createObjectURL(event.dataTransfer.files[i]);
+      var videoId = "video" + i;
+      createVideoPlayer(videoId, url);
+      pauseVideo(videoId); // Pause the video initially
+    }
+  }
+});
+
+
+
+// Bottom controls
 const checkboxes = document.getElementById("checkboxes");
-const otherVideos = document.getElementById("other-videos");
 const playButton = document.getElementById("play");
 const pauseButton = document.getElementById("pause");
 const firstFrameButton = document.getElementById("first-frame");
@@ -26,15 +163,6 @@ const increaseSpeedButton = document.getElementById("increase-speed");
 const decreaseSpeedButton = document.getElementById("decrease-speed");
 const stopButton = document.getElementById("stop");
 
-function createVideoPlayer(videoId) {
-  var videoPlayer = document.createElement("video");
-  videoPlayer.id = videoId;
-  videoPlayer.src = videos.find(video => video.id === videoId).src;
-  videoPlayer.controls = false; // Show video controls
-
-  otherVideos.appendChild(videoPlayer);
-  videoPlayers[videoId] = videoPlayer;
-}
 
 function playVideo(videoId) {
   if (videoPlayers[videoId]) {
